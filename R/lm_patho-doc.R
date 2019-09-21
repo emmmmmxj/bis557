4bcf19337127597830edf519adead60067a10311
Xiaojing Zhu <xiaojing.zhu@yale.edu>
2019-09-21 15:59:37 -0400

    Xiaojing Zhu homework 1

---
+++ R/lm_patho-doc.R
@@ -0,0 +1,11 @@
+#' lm_patho data within the linearmodel package
+#'
+#' A dataset containing extreme cases for fitting a linear regression model
+#'
+#' @format A data frame with 2 observations and 3 variables
+#' \describe{
+#'   \item{y}{The response variable}
+#'   \item{x1}{First predictor}
+#'   \item{x2}{Second predictor}
+#' }
+"lm_patho"
